3a50901f0bff9cf0d2e9b2a3e367d766d57e2558
Adam Bossy <adambossy@gmail.com>
2025-11-30 12:14:06 -0500

    Remove unused bank adapter classes and tests

--- tools/ingest/adapters/morgan_stanley.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from tools.ingest.ingest_tool import NormalizedTransaction
-from .base import BankAdapter
-
-
-class MorganStanleyAdapter(BankAdapter):
-    def parse(self, file_path: str) -> List[NormalizedTransaction]:
-        raise NotImplementedError
